67a4827724b89a3e26ee25b44d41e0a5a5f80d13
tanishkmodi <tanishkmodi6@gmail.com>
2025-01-06 23:06:46 -0500

    added functionality and comments

--- src/Roulette.java
+++ src/Roulette.java
@@ -8,13 +8,22 @@ public class Roulette implements Game{
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
+        String playAgain;
+
         System.out.println("***********************");
-        System.out.println("Welcome to Java Roulette!");
+        System.out.println("♦️♠️Welcome to Java Roulette!♦️♠️");
         System.out.println("***********************");
 
         while (!player.isBankrupt()){
 
+            System.out.println("Current Balance: $" + player.getBalance());
 
+            System.out.print("Do you want to play again (Y/N):  ");
+            playAgain = scanner.nextLine().toUpperCase();
+
+            if (!playAgain.equals("Y")){
+                break;
+            }
 
         }
 
